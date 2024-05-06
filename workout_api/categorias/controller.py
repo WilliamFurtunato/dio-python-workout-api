27c12401267e85fe63d96236a37b515b70220cdc
William Furtunato da Silva <williamfurtunato@hotmail.com>
2024-05-06 19:22:10 -0300

    feat: desafafio pt1

--- workout_api/categorias/controller.py
+++ workout_api/categorias/controller.py
@@ -6,55 +6,71 @@ from workout_api.categorias.models import CategoriaModel
 
 from workout_api.contrib.dependencies import DatabaseDependency
 from sqlalchemy.future import select
+from sqlalchemy.exc import IntegrityError
 
 router = APIRouter()
 
+
 @router.post(
-    '/', 
-    summary='Criar uma nova Categoria',
+    "/",
+    summary="Criar uma nova Categoria",
     status_code=status.HTTP_201_CREATED,
     response_model=CategoriaOut,
 )
 async def post(
-    db_session: DatabaseDependency, 
-    categoria_in: CategoriaIn = Body(...)
+    db_session: DatabaseDependency, categoria_in: CategoriaIn = Body(...)
 ) -> CategoriaOut:
     categoria_out = CategoriaOut(id=uuid4(), **categoria_in.model_dump())
     categoria_model = CategoriaModel(**categoria_out.model_dump())
-    
-    db_session.add(categoria_model)
-    await db_session.commit()
 
+    try:
+        db_session.add(categoria_model)
+        await db_session.commit()
+    except Exception as e:
+        if isinstance(e, IntegrityError):
+            raise HTTPException(
+                status_code=status.HTTP_303_SEE_OTHER,
+                detail=f"Já existe uma categoria cadastrado com o nome: {categoria_model.nome}",
+            )
+        else:
+            raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail="Ocorreu um erro ao inserir os dados no banco",
+            )
     return categoria_out
-    
-    
+
+
 @router.get(
-    '/', 
-    summary='Consultar todas as Categorias',
+    "/",
+    summary="Consultar todas as Categorias",
     status_code=status.HTTP_200_OK,
     response_model=list[CategoriaOut],
 )
 async def query(db_session: DatabaseDependency) -> list[CategoriaOut]:
-    categorias: list[CategoriaOut] = (await db_session.execute(select(CategoriaModel))).scalars().all()
-    
+    categorias: list[CategoriaOut] = (
+        (await db_session.execute(select(CategoriaModel))).scalars().all()
+    )
+
     return categorias
 
 
 @router.get(
-    '/{id}', 
-    summary='Consulta uma Categoria pelo id',
+    "/{id}",
+    summary="Consulta uma Categoria pelo id",
     status_code=status.HTTP_200_OK,
     response_model=CategoriaOut,
 )
 async def get(id: UUID4, db_session: DatabaseDependency) -> CategoriaOut:
     categoria: CategoriaOut = (
-        await db_session.execute(select(CategoriaModel).filter_by(id=id))
-    ).scalars().first()
+        (await db_session.execute(select(CategoriaModel).filter_by(id=id)))
+        .scalars()
+        .first()
+    )
 
     if not categoria:
         raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, 
-            detail=f'Categoria não encontrada no id: {id}'
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Categoria não encontrada no id: {id}",
         )
-    
+
     return categoria
